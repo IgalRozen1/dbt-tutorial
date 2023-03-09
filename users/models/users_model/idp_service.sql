33f71d5358310e2622c8ad0dc61940d3d7d9857b
Igal <igal@cypago.com>
2023-03-09 11:38:21 +0200

    Select idp mfa

--- users/models/users_model/idp_service.sql
+++ users/models/users_model/idp_service.sql
@@ -7,8 +7,8 @@ service_users as (
 ),
 
 idp_service_users as (
-    select iu.emails, iu.mfa from idp_users as iu
-    join service_users as su on iu.emails = su.emails
+    select distinct idp_users.emails, idp_users.mfa from idp_users
+    join service_users on service_users.emails = idp_users.emails
 )
 
 select * from idp_service_users
